0a80ab4d8acc1ece2cd089a8e9289883f0e711c3
ocapraro <oscar@mcapraro.com>
2026-02-20 17:55:54 -0500

    better docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import {Editor, MarkdownView, parseYaml, Plugin, stringifyYaml} from 'obsidian';
 
+const DELAY = 300;
+
 /**
  * The main plugin
  */
-
-const DELAY = 500;
-
 export default class BasesTasks extends Plugin {
   gettingTasksTimeoutID:NodeJS.Timeout;
 
@@ -13,6 +12,8 @@ export default class BasesTasks extends Plugin {
     this.registerEvent(this.app.workspace.on("editor-change", async(editor, info)=>{
       if (!(info instanceof MarkdownView))
         return;
+
+      // Leave a delay when saving tasks so it doesnt write to the not on every char change
       if(this.gettingTasksTimeoutID !== null)
         clearTimeout(this.gettingTasksTimeoutID);
       this.gettingTasksTimeoutID = setTimeout(()=>{
@@ -21,6 +22,7 @@ export default class BasesTasks extends Plugin {
     }))
   }
 
+  // Extracts the tasks from a note body and saves them as a property
   saveTasks(editor:Editor) {
     const rawFile = editor.getValue();
     const splitFile = rawFile.split("\n");
@@ -42,10 +44,11 @@ export default class BasesTasks extends Plugin {
     return parseYaml(properties);
   }
 
+  // Compares to arrays to see if each element is equal
   arraysEqual(arr1:any[], arr2:any[]):boolean {
     if(arr1.length !== arr2.length)
       return false;
-    return arr1.every((value, index) => value === arr2[index]);
+    return arr1.every((v, i) => v === arr2[i]);
   }
 }
 
