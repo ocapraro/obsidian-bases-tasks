f4bbeb01a71178cefabc176637edb55fe86ff8c8
ocapraro <25327166+ocapraro@users.noreply.github.com>
2026-03-15 16:06:22 -0400

    implement simple dnd

--- src/widgets/DragTaskWidget.ts
+++ src/widgets/DragTaskWidget.ts
@@ -1,4 +1,4 @@
-import { WidgetType } from '@codemirror/view';
+import { EditorView, WidgetType } from '@codemirror/view';
 
 export class DragTaskWidget extends WidgetType {
 
@@ -6,7 +6,7 @@ export class DragTaskWidget extends WidgetType {
     super();
   }
 
-  toDOM(): HTMLElement {
+  toDOM(view:EditorView): HTMLElement {
     const span = document.createElement('span');
     span.classList.add("task-drag-widget");
     span.draggable = true;
@@ -15,6 +15,26 @@ export class DragTaskWidget extends WidgetType {
       e.dataTransfer?.setData("text/plain", String(this.lineNumber));
       e.dataTransfer!.effectAllowed = "move";
     });
+
+    span.addEventListener("dragend", (e)=>{
+      e.preventDefault();
+      const pos = view.posAtCoords({ x: e.clientX, y: e.clientY });
+      if(!pos)
+        return;
+
+      const sourceLine = view.state.doc.line(this.lineNumber);
+      const targetLine = view.state.doc.lineAt(pos);
+      
+      if (sourceLine.number === targetLine.number) 
+        return;
+
+      view.dispatch({
+        changes:[
+          {from:sourceLine.from, to:Math.min(sourceLine.to+1, ), insert:""},
+          {from:targetLine.from, insert:sourceLine.text+"\n"}
+        ]
+      });
+    })
 
     return span;
   }
