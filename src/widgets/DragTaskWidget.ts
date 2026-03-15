23eb1903d6eb6539934f162b46e15c4182660a94
ocapraro <25327166+ocapraro@users.noreply.github.com>
2026-03-15 18:26:30 -0400

    hide line while dragging

--- src/widgets/DragTaskWidget.ts
+++ src/widgets/DragTaskWidget.ts
@@ -1,5 +1,6 @@
 import { ChangeSpec } from '@codemirror/state';
 import { EditorView, WidgetType } from '@codemirror/view';
+import { TASK_REGEX } from '../constants';
 
 export class DragTaskWidget extends WidgetType {
 
@@ -12,13 +13,22 @@ export class DragTaskWidget extends WidgetType {
     span.classList.add("task-drag-widget");
     span.draggable = true;
 
+    const lineElements = document.querySelectorAll(".cm-line");
+    const lineCount = view.state.doc.lines;
+    const targetLineElementIndex = this.lineNumber + lineElements.length - lineCount - 1;
+
     span.addEventListener("dragstart", (e) => {
       e.dataTransfer?.setData("text/plain", String(this.lineNumber));
       e.dataTransfer!.effectAllowed = "move";
+      
+      setTimeout(() => {
+        lineElements[targetLineElementIndex]?.addClass("bases-tasks-hidden");
+      }, 0);
     });
 
     span.addEventListener("dragend", (e)=>{
       e.preventDefault();
+      lineElements[targetLineElementIndex]?.removeClass("bases-tasks-hidden");
       const pos = view.posAtCoords({ x: e.clientX, y: e.clientY });
       if(!pos)
         return;
@@ -26,8 +36,10 @@ export class DragTaskWidget extends WidgetType {
       const sourceLine = view.state.doc.line(this.lineNumber);
       const targetLine = view.state.doc.lineAt(pos);
       
-      if (sourceLine.number === targetLine.number) 
+      // Don't edit if not rearranging tasks, or if no change
+      if (sourceLine.number === targetLine.number || !targetLine.text.match(TASK_REGEX)) 
         return;
+
       const changes:ChangeSpec[] = [{from:sourceLine.from, to:Math.min(sourceLine.to+1, view.state.doc.length), insert:""}];
       // If dragging from above, slot below and visa versa
       if (sourceLine.to<targetLine.from) {
